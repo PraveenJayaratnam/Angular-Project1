904311cdb17b8fc5fb3f7288fcbcd6ea19e95fa7
PraveenShaun <praveen18.warner@gmail.com>
2023-01-13 23:44:05 +0530

    third--commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,14 @@ import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { RouterModule , Routes} from '@angular/router';
 
+const routes:Routes= [
+  {path:'',component:HomeComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'login',component:LoginComponent},
+  {path:'dashboard',component:DashboardComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +25,8 @@ import { RegisterComponent } from './register/register.component';
     RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
